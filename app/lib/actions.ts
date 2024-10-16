35505727f6baa61134d58329b93f3d80ec422e89
川野辺 亮 <gggnbr@gmail.com>
2024-10-16 13:11:41 +0900

    12章まで。（学び：jsのタグ付きテンプレート文字列で、sql文を関数に渡している）

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`; // TODO:調べる タグ付きテンプレート文字列　6.6.1
+  revalidatePath("/dashboard/invoices");
+}
